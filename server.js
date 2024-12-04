0358d57e07dabe494284ecfa2a1f7522fe7ef645
SamanDev <mac.developers7@gmail.com>
2024-12-05 02:52:48 +0330

    d

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/add", async (req, res) => {
   const bet = new TopWins({
     game: req.body.game,
     userdata: JSON.stringify(req.body.userdata),
-    alldata: JSON.stringify(req.body.segments),
+    alldata: JSON.stringify(req.body.userdata),
 
   });
   const myres = await bet.save();
