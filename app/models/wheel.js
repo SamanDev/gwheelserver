79a57d13bd0497fbe6f3aad0af302c2cb4d7af40
SamanDev <mac.developers7@gmail.com>
2024-12-05 03:04:11 +0330

    d

--- app/models/wheel.js
+++ app/models/wheel.js
@@ -6,6 +6,9 @@ const TopWins = mongoose.model(
     game: { type: String, default: "" },
     userdata: { type: String, default: "" },
     alldata: { type: String, default: "" },
+    username: { type: String, default: "" },
+    x: { type: Number, default: 0 },
+    win: { type: Number, default: 0},
     
 
     date: { type: Date, default: Date.now },
